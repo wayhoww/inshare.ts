34342d6252b80246d710ac726d8354796a5d9f64
WANG Weihao <wweihao@outlook.com>
2020-04-11 19:50:31 +0800

    update README_client.md

--- webapi/setup-template.sh
+++ webapi/setup-template.sh
@@ -5,3 +5,9 @@ export WEBAPI_PROTOCOL="http";
 export WEBAPI_PORT=3000;
 export WEBAPI_LOGGER_LEVEL="develop"
 export MONGODB_URL="mongodb://localhost:27017/inshare"
+
+export CERT_KEY=`echo ~/conf/key.key`
+export CERT_CRT=`echo ~/conf/key.crt`
+
+export WEIXIN_APPID="someid"
+export WEIXIN_SECRET="somekey"
